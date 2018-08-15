fdd04b1cc0b3b4cd768b326489f51ac83f61d5eb
Paul Brabban <paul.brabban@gmail.com>
2018-08-15 20:38:52 +0100

    tidy up parse function using pattern matching

--- src/main/scala/works/tempered/examples/AccessLog.scala
+++ src/main/scala/works/tempered/examples/AccessLog.scala
@@ -8,11 +8,11 @@ object AccessLog {
   type UserId = String
   type Path = String
   type StatusCode = Int
+
   case class Entry(clientIp: ClientIp, userId: UserId, timestamp: Instant, path: Path, statusCode: StatusCode)
 
-  def parseLine(line: String): Entry = {
-    val parts = line.split(",")
-    Entry(parts(0), parts(1), new Instant(parts(2)), parts(3), parts(4).toInt)
+  def parseLine(line: String): Entry = line.split(",") match {
+    case Array(clientIp, userId, timestamp, path, statusCode) => Entry(clientIp, userId, new Instant(timestamp), path, statusCode.toInt)
   }
 
 }
